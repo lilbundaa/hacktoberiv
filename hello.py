863bea5669a0752663b5842e9a4856b8db702572
ipal <72238450+naufalsk@users.noreply.github.com>
2022-10-01 07:40:30 +0700

    ipal4
    hacktoberfest

---
+++ hello.py
@@ -0,0 +1,42 @@
+# from posixpath import split
+
+
+# print('hello world')
+# print(1 + 2)
+# print (int('10')/2)
+# print ("hello"[1])
+# print ("hello"[-1])
+
+# teks = "halo, nama saya, budi"
+# x = teks.split(", ")
+# print(x)
+
+# a = "HaLo"
+# b = "halo"
+
+# print(a.islower())
+# print(b.islower())
+
+# print( 30 * 2 + 10 - (21  // 5) )
+# print( 30 * 2 + 10 - (21  // 5) )
+
+# berat = float(input("Masukkan berat anda dalam kg: "))
+# tinggi = float(input("Masukkan tinggi anda dalam m: "))
+
+# bmi = (berat/(tinggi ** 2))
+# print("BMI Anda =",bmi)
+
+# a = 200
+# b = 300
+# d = 0
+
+# e = b // a
+# c = e ** b * d
+# print(c)
+
+# print('Result of 10 > 5: ', 10 > 5)
+
+# n = int(input('Masukan sebuah integer: '))
+# print('Apakah integer tersebut genap?', n % 2 == 0)
+
+
